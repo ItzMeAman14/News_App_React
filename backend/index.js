e4fa6447732772a43d730d592b94f539dff2d610
ItzMeAman14 <amanrehman2020@gmail.com>
2025-01-04 19:04:38 +0530

    serving react from backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 6551
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.post('/news', async (req, res) => {
     try{
         const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${req.body.country}&category=${req.body.category}&apiKey=${req.body.apiKey}&page=${req.body.page}&pagesize=${req.body.pageSize}`);
@@ -16,6 +18,10 @@ app.post('/news', async (req, res) => {
     }
   
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
   console.log('Server running on http://localhost:'+PORT);
