b79c04c1175c76a178e0e53ae2d1278b6306bd3e
ItzMeAman14 <amanrehman2020@gmail.com>
2025-01-04 19:16:28 +0530

    Created Scripts

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const path = require('path')
 const PORT = process.env.PORT || 6551
 app.use(express.json())
 app.use(cors())
